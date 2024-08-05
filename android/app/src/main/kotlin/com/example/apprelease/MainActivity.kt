94e0fb6076035050d71fd5b2b450cfef8ebfd6a4
Shokie Varun <varun.m@gokhana.com>
2024-08-05 14:09:36 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/apprelease/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.apprelease
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/values/api_end_points.dart
@@ -0,0 +1,5 @@
+class ApiEndpoints {
+  static const String url = 'https://prod.com';
+  //'https://preprod.com';
+  //'https://sandbox.com';
+}
